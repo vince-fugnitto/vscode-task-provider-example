dbd2c0de3987b3c316a22035334bd49b81071b5e
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-01-21 11:24:35 -0500

    add sleep for short tasks
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,83 +1,8 @@
 import * as vscode from 'vscode';
-
-interface TestTaskDefinition extends vscode.TaskDefinition {
-    /**
-     * The task name.
-     */
-    task: string;
-}
+import { TestTaskProvider } from './task-provider';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log(' \'vscode-task-provider-example\' extension started...');
-
-    let taskPromise: Thenable<vscode.Task[]> | undefined = undefined;
-    const taskProvider = vscode.tasks.registerTaskProvider('vince', {
-        provideTasks: () => {
-            if (!taskPromise) {
-                taskPromise = getTestTasks();
-            }
-            return taskPromise;
-        },
-        resolveTask(_task: vscode.Task): vscode.Task | undefined {
-            const task = _task.definition.task;
-            if (task) {
-                const definition: TestTaskDefinition = <any>_task.definition;
-                return new vscode.Task(
-                    definition,
-                    _task.scope ?? vscode.TaskScope.Workspace,
-                    definition.task,
-                    _task.name,
-                    _task.execution,
-                );
-            }
-            return undefined;
-        }
-    });
+    vscode.tasks.registerTaskProvider('vince', new TestTaskProvider);
 }
 
 export function deactivate() { }
-
-export function getTestTasks(): Promise<vscode.Task[]> {
-    const tasks: vscode.Task[] = [];
-    tasks.push(getTestTask());
-    tasks.push(getBuildTask());
-    tasks.push(getRebuildTask());
-    tasks.push(getCleanTask());
-    tasks.push(getNoneTask());
-    return Promise.resolve(tasks);
-}
-
-function getTestTask(): vscode.Task {
-    const workspaceFolder = vscode.workspace.workspaceFolders![0];
-    const task = new vscode.Task({ type: 'vince', task: 'vince-test' }, workspaceFolder, 'vince-test', 'vince', new vscode.ShellExecution(`echo "Hello World - Test"`));
-    task.group = vscode.TaskGroup.Test;
-    return task;
-}
-
-function getBuildTask(): vscode.Task {
-    const workspaceFolder = vscode.workspace.workspaceFolders![0];
-    const task = new vscode.Task({ type: 'vince', task: 'vince-build' }, workspaceFolder, 'vince-build', 'vince', new vscode.ShellExecution(`echo "Hello World - Build"`));
-    task.group = vscode.TaskGroup.Build;
-    return task;
-}
-
-function getRebuildTask(): vscode.Task {
-    const workspaceFolder = vscode.workspace.workspaceFolders![0];
-    const task = new vscode.Task({ type: 'vince', task: 'vince-rebuild' }, workspaceFolder, 'vince-rebuild', 'vince', new vscode.ShellExecution(`echo "Hello World - Rebuild"`));
-    task.group = vscode.TaskGroup.Rebuild;
-    return task;
-}
-
-function getCleanTask(): vscode.Task {
-    const workspaceFolder = vscode.workspace.workspaceFolders![0];
-    const task = new vscode.Task({ type: 'vince', task: 'vince-clean' }, workspaceFolder, 'vince-clean', 'vince', new vscode.ShellExecution(`echo "Hello World - Clean"`));
-    task.group = vscode.TaskGroup.Clean;
-    return task;
-}
-
-function getNoneTask(): vscode.Task {
-    const workspaceFolder = vscode.workspace.workspaceFolders![0];
-    const task = new vscode.Task({ type: 'vince', task: 'vince-none' }, workspaceFolder, 'vince-none', 'vince', new vscode.ShellExecution(`echo "Hello World - None"`));
-    return task;
-}
-
---
+++ src/task-provider.ts
@@ -0,0 +1,63 @@
+import * as vscode from 'vscode';
+
+export interface TestTaskDefinition extends vscode.TaskDefinition {
+    /**
+     * The task name.
+     */
+    task: string;
+}
+
+export class TestTaskProvider implements vscode.TaskProvider {
+
+    provideTasks(_token: vscode.CancellationToken): vscode.ProviderResult<vscode.Task[]> {
+        return getTestTasks();
+    }
+
+    resolveTask(task: vscode.Task, _token: vscode.CancellationToken): vscode.Task | undefined {
+        return task;
+    }
+}
+
+export function getTestTasks(): Promise<vscode.Task[]> {
+    const tasks: vscode.Task[] = [];
+    tasks.push(getTestTask());
+    tasks.push(getBuildTask());
+    tasks.push(getRebuildTask());
+    tasks.push(getCleanTask());
+    tasks.push(getNoneTask());
+    return Promise.resolve(tasks);
+}
+
+function getTestTask(): vscode.Task {
+    const workspaceFolder = vscode.workspace.workspaceFolders![0];
+    const task = new vscode.Task({ type: 'vince', task: 'vince-test' }, workspaceFolder, 'vince-test', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Test"`));
+    task.group = vscode.TaskGroup.Test;
+    return task;
+}
+
+function getBuildTask(): vscode.Task {
+    const workspaceFolder = vscode.workspace.workspaceFolders![0];
+    const task = new vscode.Task({ type: 'vince', task: 'vince-build' }, workspaceFolder, 'vince-build', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Build"`));
+    task.group = vscode.TaskGroup.Build;
+    return task;
+}
+
+function getRebuildTask(): vscode.Task {
+    const workspaceFolder = vscode.workspace.workspaceFolders![0];
+    const task = new vscode.Task({ type: 'vince', task: 'vince-rebuild' }, workspaceFolder, 'vince-rebuild', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Rebuild"`));
+    task.group = vscode.TaskGroup.Rebuild;
+    return task;
+}
+
+function getCleanTask(): vscode.Task {
+    const workspaceFolder = vscode.workspace.workspaceFolders![0];
+    const task = new vscode.Task({ type: 'vince', task: 'vince-clean' }, workspaceFolder, 'vince-clean', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Clean"`));
+    task.group = vscode.TaskGroup.Clean;
+    return task;
+}
+
+function getNoneTask(): vscode.Task {
+    const workspaceFolder = vscode.workspace.workspaceFolders![0];
+    const task = new vscode.Task({ type: 'vince', task: 'vince-none' }, workspaceFolder, 'vince-none', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - None"`));
+    return task;
+}
