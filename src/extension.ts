3542c98b1153d524f50c86d1548100688008c62e
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-03-23 08:17:11 -0400

    task: add presentationOptions.reveal
    The following commit adds `presentationOptions.reveal` to the task after
    constructing it.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,9 @@ import * as vscode from 'vscode';
 import { TestTaskProvider } from './task-provider';
 
 export function activate(context: vscode.ExtensionContext) {
-    vscode.tasks.registerTaskProvider('vince', new TestTaskProvider);
+    if (vscode.workspace.workspaceFolders?.length) {
+        vscode.tasks.registerTaskProvider('vince', new TestTaskProvider);
+    }
 }
 
 export function deactivate() { }
--- src/task-provider.ts
+++ src/task-provider.ts
@@ -32,6 +32,7 @@ function getTestTask(): vscode.Task {
     const workspaceFolder = vscode.workspace.workspaceFolders![0];
     const task = new vscode.Task({ type: 'vince', task: 'vince-test' }, workspaceFolder, 'vince-test', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Test"`));
     task.group = vscode.TaskGroup.Test;
+    task.presentationOptions.reveal = vscode.TaskRevealKind.Always;
     return task;
 }
 
@@ -39,6 +40,7 @@ function getBuildTask(): vscode.Task {
     const workspaceFolder = vscode.workspace.workspaceFolders![0];
     const task = new vscode.Task({ type: 'vince', task: 'vince-build' }, workspaceFolder, 'vince-build', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Build"`));
     task.group = vscode.TaskGroup.Build;
+    task.presentationOptions.reveal = vscode.TaskRevealKind.Always;
     return task;
 }
 
@@ -46,6 +48,7 @@ function getRebuildTask(): vscode.Task {
     const workspaceFolder = vscode.workspace.workspaceFolders![0];
     const task = new vscode.Task({ type: 'vince', task: 'vince-rebuild' }, workspaceFolder, 'vince-rebuild', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Rebuild"`));
     task.group = vscode.TaskGroup.Rebuild;
+    task.presentationOptions.reveal = vscode.TaskRevealKind.Always;
     return task;
 }
 
@@ -53,11 +56,13 @@ function getCleanTask(): vscode.Task {
     const workspaceFolder = vscode.workspace.workspaceFolders![0];
     const task = new vscode.Task({ type: 'vince', task: 'vince-clean' }, workspaceFolder, 'vince-clean', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - Clean"`));
     task.group = vscode.TaskGroup.Clean;
+    task.presentationOptions.reveal = vscode.TaskRevealKind.Always;
     return task;
 }
 
 function getNoneTask(): vscode.Task {
     const workspaceFolder = vscode.workspace.workspaceFolders![0];
     const task = new vscode.Task({ type: 'vince', task: 'vince-none' }, workspaceFolder, 'vince-none', 'vince', new vscode.ShellExecution(`sleep 5; echo "Hello World - None"`));
+    task.presentationOptions.reveal = vscode.TaskRevealKind.Always;
     return task;
 }
